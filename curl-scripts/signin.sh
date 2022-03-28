de5b02c5d6acaa386a8cf60cb79f4540abea82fd
avongalie <alexsiaavila@gmail.com>
2022-03-28 09:39:23 -0400

    set up curl scripts

---
+++ curl-scripts/signin.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+
+curl "https://tic-tac-toe-api-development.herokuapp.com/sign-in" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --data '{
+    "credentials": {
+      "email": "'"${EMAIL}"'",
+      "password": "'"${PASSWORD}"'"
+    }
+  }'
+
+echo
