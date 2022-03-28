03727e367a9d898e3568a7aea16033ab40f89e6b
avongalie <alexsiaavila@gmail.com>
2022-03-28 10:34:23 -0400

    set up API calls

--- app/auth/api.js
+++ app/auth/api.js
@@ -8,6 +8,69 @@ const signUp = function(data) {
     })
 }
 
+const signIn = function(data) {
+    return $.ajax({
+        method: 'POST',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-in',
+        data
+    })
+}
+
+const signOut = function() {
+    return $.ajax({
+        method: 'DELETE',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/sign-up',
+        headers: {
+            Authorization: 'Bearer ' + store.user.token
+        }
+    })
+}
+
+const changePassword = function(data) {
+    return $.ajax({
+        method: 'PATCH',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/change-password',
+        headers: {
+            Authorization: 'Bearer ' + store.user.token
+        },
+        data
+    })
+}
+
+const newGame = function() {
+    return $.ajax({
+        method: 'POST',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/games',
+        headers: {
+            Authorization: 'Bearer ' + store.user.token
+        }
+    })
+}
+
+const updateGame = function(index, value, gameStatus) {
+    return $.ajax({
+        method: 'PATCH',
+        url: 'https://tic-tac-toe-api-development.herokuapp.com/games/' + store.user.id,
+        headers: {
+            Authorization: 'Bearer ' + store.user.token
+        },
+    data:{
+        "game": {
+            "cell": {
+              "index": index,
+              "value": value
+            },
+            "over": gameStatus
+          }
+    }
+    })
+}
+
 module.exports = {
-    signUp
+    signUp,
+    signIn,
+    signOut,
+    changePassword,
+    newGame,
+    updateGame
 }
